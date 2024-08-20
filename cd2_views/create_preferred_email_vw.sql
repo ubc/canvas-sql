98bb320f7c25db62d0726eeabd0dfe3b985a2738
Craig Thompson <craig.thompson@ubc.ca>
2024-08-20 15:51:49 -0700

    added script to create preferred email view.

---
+++ cd2_views/create_preferred_email_vw.sql
@@ -0,0 +1,20 @@
+CREATE VIEW canvas.preferred_email_vw as
+SELECT
+  comm1.user_id,
+  comm1.path as email
+FROM
+  canvas.communication_channels comm1
+  JOIN (
+    SELECT
+      user_id,
+      MIN(position) AS min_position
+    FROM
+      canvas.communication_channels comm2
+    WHERE
+      path_type = 'email'
+      AND workflow_state = 'active'
+    GROUP BY
+      user_id
+    ) AS min_positions
+    ON comm1.user_id = min_positions.user_id
+    AND comm1.position = min_positions.min_position
